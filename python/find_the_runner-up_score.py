77da4b68702d7d344245c525f30dcd4249701572
David Siddle <siddav83@gmail.com>
2023-03-28 22:45:22 +0100

    completed find_the_runner-up_score

---
+++ python/find_the_runner-up_score.py
@@ -0,0 +1,15 @@
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = map(int, input().split())
+    
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+import random
+
+arr = [2,3,6,6,5]
+# for i in range(5):
+#     arr.append(random.randint(0,5))
+sort = sorted(set(arr))
+print(sort.pop())
+# print(sort.pop()
+print(sort.pop())
+
